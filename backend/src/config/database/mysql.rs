a50dd8c588e63df1d8c938647e50b41bb4c75fc5
Pierre MARCADE <pierre.marcade@upyne.com>
2024-07-30 13:04:12 +0200

    fix: problem on mysql and sqlite config

--- backend/src/config/database/mysql.rs
+++ backend/src/config/database/mysql.rs
@@ -7,19 +7,19 @@ pub struct Connector;
 impl Connector {
     pub fn connect(database_url: &String) -> Result<Pool<ConnectionManager<MysqlConnection>>, Error> {
         let manager = ConnectionManager::<MysqlConnection>::new(database_url);
-        let pool = Pool::builder()
+        Pool::builder()
             .test_on_check_out(true)
-            .build(manager);
-        pool
+            .build(manager)
     }
 }
 
+#[derive(Debug, Clone)]
 pub struct Database {
-    pub pool: MysqlConnection
+    pub pool: Pool<ConnectionManager<MysqlConnection>>
 }
 
 impl Database {
-    pub fn new(pool: MysqlConnection) -> Self {
+    pub fn new(pool: Pool<ConnectionManager<MysqlConnection>>) -> Self {
         Database { pool }
     }
 }
