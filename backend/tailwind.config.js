1ee4c4e0acddb6c787d23245b672c644bef671c2
Pierre Marcadé <pierremarcade@gmail.com>
2024-09-09 22:44:16 +0200

    feat: add barkeel logo

--- backend/tailwind.config.js
+++ backend/tailwind.config.js
@@ -1,12 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        barkeel: '#ff5555',
+      },
+    },
+  },
   content: ["./src/**/*.{html,js}"],
   plugins: [
     require('@tailwindcss/typography'),
     require('@tailwindcss/forms'),
     require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/container-queries'),
-  ],
+  ]
 }
 
